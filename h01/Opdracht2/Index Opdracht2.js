fc150c0f953e5b63653f4b263d869486fc2901a3
Paul <paul.vanbreda@student.rocvf.nl>
2019-12-02 10:52:19 +0100

    h01 af

---
+++ h01/Opdracht2/Index Opdracht2.js
@@ -0,0 +1,8 @@
+var a = 10;
+if (a>9){
+    console.log("Hallo a is groter dan 9 hoor");
+}
+
+for (var i=0; i<10;i++){
+    console.log("i heeft nu de waarde" +i);
+}
---
+++ h01/Opdracht3/index Opdracht3.js
@@ -0,0 +1,7 @@
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalwielen: 4,
+    kleur: "blauw"
+}
+console.log(mijnauto.kleur)
