2e5a3a322c66a195333c7a1128cc078dcb0e2427
Paul <paul.vanbreda@student.rocvf.nl>
2019-12-03 10:35:38 +0100

    h02 af

---
+++ h02/Opdracht1/vulmetjs.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText ="Mijn geweldige pagina";
+document.getElementById("subkop").innerText ="Mijn geweldige subkop";
+document.getElementById("aap1").scr = "img/aap1.jpg";
+document.getElementById("aap2").scr = "img/aap2.jpg";
