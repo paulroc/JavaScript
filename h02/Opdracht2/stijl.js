97d5924469b08c37c7138c46fb32217327c5ac2f
Paul <paul.vanbreda@student.rocvf.nl>
2020-02-03 10:25:51 +0100

    klaaaar
    misschien

---
+++ h02/Opdracht2/stijl.js
@@ -0,0 +1,18 @@
+var Blokje1 = {
+    kleur: "blauw"
+}
+var Blokje2 = {
+    kleur: "groen"
+}
+var Blokje3 = {
+    kleur: "geel"
+}
+var Blokje4 = {
+    kleur: "rood"
+}
+
+
+document.getElementById("bl1").innerText = Blokje1
+document.getElementById("bl2").innerText  = Blokje2
+document.getElementById("bl3").innerText  = Blokje3
+document.getElementById("bl4").innerText  = Blokje4
